a4072a1b210b5af1204fa554d6740c9c417ce957
Outgoingkarma <ernestastyl@gmail.com>
2025-11-11 14:33:32 +0200

    Updated `GameConfig` settings to adjust action count, random barricades, and game speed; marked `SkipCommand` as final for improved code safety.

--- src/main/java/com/example/vulkanoevakuacija/Game/GameConfig.java
+++ src/main/java/com/example/vulkanoevakuacija/Game/GameConfig.java
@@ -3,7 +3,7 @@ package com.example.vulkanoevakuacija.Game;
 public final class GameConfig {
     private GameConfig() {}
 
-    public static final int PLAYER_TOTAL_ACTIONS_COUNT = 0;
+    public static final int PLAYER_TOTAL_ACTIONS_COUNT = 1;
 
     public static final char ROAD = '.';
     public static final char WALL = '■';
@@ -16,17 +16,17 @@ public final class GameConfig {
     public static final char BARRICADE = 'O';
     public static final char RESIDENT_SLOW = '☺';
     public static final char RESIDENT_FAST = '⚡';
-    public static final char RESIDENT_EVACUATED = '✔';
+    //public static final char RESIDENT_EVACUATED = '✔';
     public static final char RESIDENT_DEAD = '☠';
 
 
     public static final int rows = 8, cols = 18,
-            houses = 3, safeZones = 2, randomBarricades = 50, randomWalls = 20,
+            houses = 3, safeZones = 2, randomBarricades = 30, randomWalls = 20,
             lavaSeeds = 1,
 
             BARRICADE_HP = 3;
 
-    public static final int gameSpeed = 33;
+    public static final int gameSpeed = 1000;
 
 
     public static final String[] DEFAULT_MAP = {
--- src/main/java/com/example/vulkanoevakuacija/actions/SkipCommand.java
+++ src/main/java/com/example/vulkanoevakuacija/actions/SkipCommand.java
@@ -2,7 +2,7 @@ package com.example.vulkanoevakuacija.actions;
 
 import com.example.vulkanoevakuacija.strategy.GameContext;
 
-public class SkipCommand implements Command {
+public final class SkipCommand implements Command {
     @Override
     public boolean execute(GameContext ctx) {
         System.out.println("Skipped turn");
